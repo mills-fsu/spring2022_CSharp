6091bfff40f47c04ffccf1893c51a979eeb93d8c
crmills <chris.mills0905@gmail.com>
2022-01-14 14:37:02 -0500

    initial commit

---
+++ ListManagement/ListManagement/Program.cs
@@ -0,0 +1,99 @@
+﻿
+using ListManagement.models;
+using System2 = System;
+
+namespace ListManagement // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var todos = new List<ToDo>();
+            Console.WriteLine("Welcome to the List Management App");
+
+            var nextTodo = new ToDo();
+            PrintMenu();
+
+            int input = -1;
+            if(int.TryParse(Console.ReadLine(),out input)) {
+                while (input != 3) //==
+                {
+                    nextTodo = new ToDo();
+                    if (input == 1)
+                    {
+                        //C - Create
+                        //ask for property values
+                        Console.WriteLine("Give me a Name");
+                        nextTodo.Name = Console.ReadLine();
+
+
+                        //todos.Add(nextTodo);
+
+                        //nextTodo.Deadline = DateTime.TryParse(Console.ReadLine());
+                    }
+                    else if (input == 2)
+                    {
+                        //D - Delete/Remove
+                        //Console.WriteLine("Which string should I delete?");
+                        //var strIndex = int.Parse(Console.ReadLine());
+                        //stringList.RemoveAt(strIndex - 1);
+                    } 
+                    else if (input == 3)
+                    {
+                        //U - Update/Edit
+                    }
+                    else if (input == 4)
+                    {
+                        //Complete Task
+                    }
+                    else if(input ==5)
+                    {
+                        //R - Read / List uncompleted tasks
+
+                    } else if (input ==6)
+                    {
+                        //R - Read / List all tasks
+                        foreach(var todo in todos)
+                        {
+                            Console.WriteLine(todo.ToString());
+                        }
+                    } else if (input == 7)
+                    {
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("I don't know what you mean");
+                    }
+
+                    PrintMenu();
+                    input = int.Parse(Console.ReadLine());
+                }
+            }
+           else
+            {
+                Console.WriteLine("User did not specify a valid int!");
+            }
+            
+
+            foreach(var item in stringList)
+            {
+                Console.WriteLine(item);
+            }
+           
+            Console.ReadLine();
+        }
+
+        public static void PrintMenu()
+        {
+            Console.WriteLine("1. Add Item");
+            Console.WriteLine("2. Delete Item");
+            Console.WriteLine("3. Exit");
+        }
+
+        public static void AddString(List<string> strList, string str)
+        {
+            strList.Add(str);
+        }
+    }
+}
---
+++ ListManagement/ListManagement/models/ToDo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListManagement.models
+{
+    public class ToDo
+    {
+        private string name;
+
+        //C#-style properties
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if(name != value)
+                {
+                    name = value;
+                }
+            }
+        }
+
+        public string Description { get; set; }
+        public DateTime Deadline { get; set; }
+        public bool IsCompleted { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Description} Completed: {IsCompleted}";
+        }
+    }
+}
